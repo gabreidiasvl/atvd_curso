6322e00a486b3b09c767bf9f900fbb71a8a65b0a
gabreidiasvl <gabrieldiasvale25@gmail.com>
2024-05-29 16:44:25 -0300

    Add files via upload

---
+++ atividade aula 1 curso.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+cout << "
+______________      ✻ ═══════ •❅• ═══════ ✼
+│             │    ┃                        ┃
+│             │    ┃    INSIRA UMA MOEDA    ┃
+│   ⭐    ⭐    │    ┃                       ┃
+│             │     ✻ ═══════ •❅• ═══════ ✼
+│             │                 │
+│     U       │                 │
+│_____________│                 │
+"
+//o desenho é um rapaz segurando uma placa sinalizando o jogador a inserir a moeda by: gabriel dias
+
+return 0;
+}
